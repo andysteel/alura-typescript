06a4290990bb56a7b26162709adcb492286d40eb
Anderson Dias <andersoninfonet@gmial.com>
2023-05-24 11:31:33 -0300

    refatoração

--- dist/js/models/negociacao.js
+++ dist/js/models/negociacao.js
@@ -1,22 +1,17 @@
 export class Negociacao {
     _data;
-    _quantidade;
-    _valor;
-    constructor(data, quantidade, valor) {
-        this._data = data;
-        this._quantidade = quantidade;
-        this._valor = valor;
-    }
-    get data() {
-        return this._data;
-    }
-    get quantidade() {
-        return this._quantidade;
-    }
-    get valor() {
-        return this._valor;
+    quantidade;
+    valor;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
     }
     get volume() {
-        return this._quantidade * this._valor;
+        return this.quantidade * this.valor;
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
     }
 }
