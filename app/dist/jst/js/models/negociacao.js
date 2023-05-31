13c72e99a390e829590b3a68db1289c1073fe3b4
Anderson Dias <andersoninfonet@gmial.com>
2023-05-30 21:10:48 -0300

    alterando estrutura do projeto

---
+++ app/dist/jst/js/models/negociacao.js
@@ -0,0 +1,20 @@
+export class Negociacao {
+    _data;
+    quantidade;
+    valor;
+    constructor(_data, quantidade, valor) {
+        this._data = _data;
+        this.quantidade = quantidade;
+        this.valor = valor;
+    }
+    get volume() {
+        return this.quantidade * this.valor;
+    }
+    get data() {
+        const data = new Date(this._data.getTime());
+        return data;
+    }
+    static criaDe(dataString, qtdString, valorString) {
+        return new Negociacao(new Date(dataString.replace(/-/g, ',')), Number(qtdString), Number(valorString));
+    }
+}
---
+++ app/dist/jst/js/views/negociacoes-view.js
@@ -0,0 +1,27 @@
+import { View } from "./view.js";
+export class NegociacoesView extends View {
+    template(model) {
+        return `
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>DATA</th>
+                        <th>QUANTIDADE</th>
+                        <th>VALOR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    ${model.lista().map(negociacao => {
+            return `
+                            <tr>
+                                <td>${negociacao.data.toLocaleDateString()}</td>
+                                <td>${negociacao.quantidade}</td>
+                                <td>${negociacao.valor.toLocaleString('pt-BR')}</td>
+                            </tr>
+                        `;
+        }).join('')}
+                </tbody>
+            </table>
+        `;
+    }
+}
